6dee93ce76134ac71b235155bce2ab677fe3719f
oxyflour <oxyflour@hotmail.com>
2022-09-21 00:09:47 +0800

    add shape.meta.ColorRGB & LayerName

--- src/step/step.cc
+++ src/step/step.cc
@@ -10,16 +10,94 @@
 #include <TopAbs_ShapeEnum.hxx>
 #include <Standard_CString.hxx>
 #include <StepShape_ManifoldSolidBrep.hxx>
+#include <StepVisual_StyledItem.hxx>
+#include <StepVisual_PresentationLayerAssignment.hxx>
+#include <StepVisual_StyledItem.hxx>
+#include <StepVisual_PresentationStyleSelect.hxx>
+#include <StepVisual_PresentationStyleAssignment.hxx>
+#include <StepVisual_SurfaceStyleUsage.hxx>
+#include <StepVisual_SurfaceSideStyle.hxx>
+#include <StepVisual_SurfaceStyleFillArea.hxx>
+#include <StepVisual_FillAreaStyle.hxx>
+#include <StepVisual_FillAreaStyleColour.hxx>
+#include <StepVisual_Colour.hxx>
+#include <StepVisual_ColourRgb.hxx>
+#include <Transfer_TransientProcess.hxx>
+#include <TransferBRep.hxx>
 
 #include "../topo/shape.h"
 
-auto GetSolidNames(STEPControl_Reader &reader) {
-    TopExp_Explorer exp;
+auto UpdateMeta(STEPControl_Reader &reader) {
+    auto model = reader.WS()->Model();
     auto trans = reader.WS()->TransferReader();
+    // https://github.com/Open-Cascade-SAS/OCCT/blob/fd5c113a0367cc5e0b086544f2e900265545aa72/src/STEPCAFControl/STEPCAFControl_Reader.cxx#L1468
+    auto &proc = trans->TransientProcess();
+    for (int i = 0; i < model->NbEntities(); i ++) {
+        auto ent = model->Value(i + 1);
+        if (ent->IsKind(StepVisual_PresentationLayerAssignment::get_type_descriptor())) {
+            auto layer = Handle(StepVisual_PresentationLayerAssignment)::DownCast(ent);
+            for (int i = 0; i < layer->NbAssignedItems(); i ++) {
+                auto item = layer->AssignedItemsValue(i + 1);
+                auto bind = proc->Find(item.Value());
+                auto shape = TransferBRep::ShapeResult(proc, bind);
+                auto &meta = Shape::MetaMap[shape.HashCode(0x0fffffff)];
+                meta["LayerName"] = layer->Name()->ToCString();
+                meta["LayerDescription"] = layer->Description()->ToCString();
+            }
+        } else if (ent->IsKind(StepVisual_StyledItem::get_type_descriptor())) {
+            auto style = Handle(StepVisual_StyledItem)::DownCast(ent);
+            auto bind = proc->Find(style->Item());
+            if (bind.IsNull()) {
+                continue;
+            }
+            auto shape = TransferBRep::ShapeResult(proc, bind);
+            if (shape.ShapeType() != TopAbs_ShapeEnum::TopAbs_SOLID) {
+                continue;
+            }
+            for (int i = 0; i < style->NbStyles(); i ++) {
+                auto item = style->StylesValue(i + 1);
+                for (int j = 0; j < item->NbStyles(); j ++) {
+                    auto value = item->StylesValue(j + 1);
+                    auto usage = value.SurfaceStyleUsage();
+                    if (usage.IsNull()) {
+                        continue;
+                    }
+                    auto style = usage->Style();
+                    for (int k = 0; k < style->NbStyles(); k ++) {
+                        auto item = style->StylesValue(k + 1);
+                        auto fill = item.SurfaceStyleFillArea();
+                        if (fill.IsNull()) {
+                            continue;
+                        }
+                        auto area = fill->FillArea();
+                        if (area.IsNull()) {
+                            continue;
+                        }
+                        for (int u = 0; u < area->NbFillStyles(); u ++) {
+                            auto item = area->FillStylesValue(u + 1);
+                            auto color = item.FillAreaStyleColour()->FillColour();
+                            auto rgb = Handle(StepVisual_ColourRgb)::DownCast(color);
+                            auto &meta = Shape::MetaMap[shape.HashCode(0x0fffffff)];
+                            meta["ColorRGB"] =
+                                std::to_string(rgb->Red()) + "," +
+                                std::to_string(rgb->Green()) + "," +
+                                std::to_string(rgb->Blue());
+                            // WTF
+                            break;
+                            break;
+                            break;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    TopExp_Explorer exp;
     for (exp.Init(reader.Shape(), TopAbs_ShapeEnum::TopAbs_SOLID); exp.More(); exp.Next()) {
         auto &shape = exp.Current();
         auto ent = trans->EntityFromShapeResult(shape, 1);
-        if (!ent.IsNull()) {
+        if (!ent.IsNull() && ent->IsKind(StepShape_ManifoldSolidBrep::get_type_descriptor())) {
             auto prop = Handle(StepShape_ManifoldSolidBrep)::DownCast(ent);
             auto &meta = Shape::MetaMap[shape.HashCode(0x0fffffff)];
             meta["ManifoldSolidBrep"] = prop->Name()->ToCString();
@@ -37,7 +115,7 @@ Napi::Value LoadStep(const Napi::CallbackInfo &info) {
         return info.Env().Undefined();
     } else {
         reader.TransferRoot();
-        GetSolidNames(reader);
+        UpdateMeta(reader);
         return Shape::Create(reader.Shape());
     }
 }
